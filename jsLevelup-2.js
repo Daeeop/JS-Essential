c270e9ad6222c89fe4a45f7273282c51659fc9ca
김대업 <rlaeodjq681@gmail.com>
2021-08-19 23:01:23 +0900

    구조분해 할당(비구조화)

--- jsLevelup-2.js
+++ jsLevelup-2.js
@@ -50,9 +50,42 @@ const keys = Object.keys(user)
 console.log(keys) // ['name' , 'age' ,'email'] 객체 데이터의 key 속성명을 배열로 만들어서 반환
 
 // 활용
-console.log(keys[2]); // email
-console.log(user['email']) // daeeop94daum.net
+console.log(keys[2]); // email 
+console.log(user['email']) // daeeop94daum.net // 이런식으로 접근가능 
 
 const values = keys.map(item => user[item])
 console.log(values)
 
+
+
+
+// 구조 분해 할당 (비구조화 할당)
+const admin = {
+  name : 'Heropy',
+  age : 85,
+  email : 'daeeop94@naver.com'
+}
+
+const { name , age , email , address} = admin
+console.log(`관리자 이름 : ${name}`)
+console.log(`${name}의 나이는 ${age}`)
+console.log(`${name}의 이메일 주소는 ${email}`)
+console.log(address); // undefined
+// const {address = 'korea'} 이런 식으로 없는 속성에 대해서는 기본값을 지정 해줄 수 있다. (속성이 존재하면 기존의 속성이 반환)
+// const {name: heropy} = admin 이런식으로 사용 하면 admin의 name 속성을 heropy라는 변수로 사용 할 수 있다.
+
+// admin이라는 객체 데이터의 구조를 분해해서 내가 원하는 속성만 꺼내서 사용 할 수 있는 개념 
+// 구조분해된 내용을 변수로 만들어서 활용 할 수 있다.
+// const email = admin.email / admin['email]과 같은 효과
+// 좋은점 : 속성이 많은 경우에 필요한것만 꺼내서 사용 할 수 있는 장점이 있다.
+
+// 구조분해 할당은 배열에서도 가능 하다.
+const countrys = ['USA' , 'KOR' , 'ENG']
+const [one , two , three ,four] = countrys // [] 사용주의 !! 아이템이 순서대로 할당 된다.
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(four); // undefined
+
+const [,kor] = countrys
+console.log(kor);
